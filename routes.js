46cb89428943eb5667b2fde3fd07bc48e885f94c
thiago-burnatt <thiago-burnatt@hotmail.com>
2022-03-21 22:04:06 -0300

    .

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ const multerConfig = require('./src/middlewares/multerConfig');
 const downloadController = require('./src/controllers/downloadController');
 
 // Rotas da home
-route.get('/',homeController.homePage); // Não executa a função
-route.post('/', multerConfig, homeController.fileConversion);
+route.get('/',homeController.homePage);
+route.post('/download', multerConfig, homeController.fileConversion);
 
 route.get('/thanks', downloadController.downloadIndex);
 
